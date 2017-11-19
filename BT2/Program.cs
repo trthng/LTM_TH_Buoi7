55050a27d5f6c22e55600a54535b24450ebaac94
nguyenle510 <trthng96@gmail.com>
2017-11-19 22:27:42 +0700

    Add project files.

---
+++ BT2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien hoac ip: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+
+            // In tên miền hoặc bí danh
+            foreach (string s in e.Aliases)
+                Console.WriteLine("\t+ Alias: {0} \n", s);
+
+            // Danh sách điạ chỉ IP
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t+ IP: {0}", i);
+            Console.WriteLine();
+        }
+    }
+}
+
